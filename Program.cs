19e2955050d4426be2024a87159017ae77d0954f
Franco Pereira <56894200+franpereira@users.noreply.github.com>
2020-03-31 19:56:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GroupTesting2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Soy Fran :P");
         }
     }
 }
